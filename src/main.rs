461d5bce10c7e40411b96ee3cb0d01b8574e8a1d
Amjad Jibon <amjad.jibon@gmail.com>
2022-03-25 17:03:16 +0600

    echo hyper update

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,27 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use futures::TryStreamExt as _;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::{Body, Request, Response, Server, StatusCode, header};
+use log::{debug, error, info};
 
-// async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-//     Ok(Response::new("Hello, World".into()))
-// }
-
-/// This is our service handler. It receives a Request, routes on its
-/// path, and returns a Future of a Response.
 async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
-        // Serve some instructions at /
-        (&Method::GET, "/") => Ok(Response::new(Body::from(
-            "Hello, World!",
-        ))),
-
-        // Simply echo the body back to the client.
-        (&Method::POST, "/echo") => Ok(Response::new(req.into_body())),
-
-        // Convert to uppercase before sending back to client using a stream.
-        (&Method::POST, "/echo/uppercase") => {
-            let chunk_stream = req.into_body().map_ok(|chunk| {
-                chunk
-                    .iter()
-                    .map(|byte| byte.to_ascii_uppercase())
-                    .collect::<Vec<u8>>()
-            });
-            Ok(Response::new(Body::wrap_stream(chunk_stream)))
-        }
-
-        // Reverse the entire body before sending back to the client.
-        //
-        // Since we don't know the end yet, we can't simply stream
-        // the chunks as they arrive as we did with the above uppercase endpoint.
-        // So here we do `.await` on the future, waiting on concatenating the full body,
-        // then afterwards the content can be reversed. Only then can we return a `Response`.
-        (&Method::POST, "/echo/reversed") => {
-            let whole_body = hyper::body::to_bytes(req.into_body()).await?;
-
-            let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
-            Ok(Response::new(Body::from(reversed_body)))
-        }
-
-        // Return the 404 Not Found for other routes.
         _ => {
-            let mut not_found = Response::default();
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
+            debug!("Path: {}", req.uri());
+            debug!("Method: {}", req.method());
+            debug!("Headers: {:?}", req.headers());
+            debug!("Body: {:?}", hyper::body::to_bytes(req.into_body()).await?);
+
+            let response = Response::builder()
+                .status(StatusCode::OK)
+                .header(header::CONTENT_TYPE, "application/json")
+                .body(Body::from("{}")).unwrap();
+            Ok(response)
         }
     }
 }
 
 async fn shutdown_signal() {
-    // Wait for the CTRL+C signal
     tokio::signal::ctrl_c()
         .await
         .expect("failed to install CTRL+C signal handler");
@@ -62,6 +29,7 @@ async fn shutdown_signal() {
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     // We'll bind to 127.0.0.1:3000
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
@@ -74,18 +42,13 @@ async fn main() {
 
     let server = Server::bind(&addr).serve(make_svc);
 
-    // Run this server for... forever!
-    // if let Err(e) = server.await {
-    //     eprintln!("server error: {}", e);
-    // }
-
     // And now add a graceful shutdown signal...
     let graceful = server.with_graceful_shutdown(shutdown_signal());
 
     // Run this server for... forever!
-    println!("server running on http://{}", addr);
+    info!("server running on http://{}", addr);
     if let Err(e) = graceful.await {
-        eprintln!("server error: {}", e);
+        error!("server error: {}", e);
     }
-    println!(" gracefully shutdown complete")
+    info!(" gracefully shutdown complete")
 }
