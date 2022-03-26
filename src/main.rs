4a2e3a84c1451e8afcda582a026c1a2b27e28607
Amjad Jibon <amjad.jibon@gmail.com>
2022-03-26 13:49:48 +0600

    optimized code

--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,46 @@ use std::convert::Infallible;
 use std::env;
 use std::net::SocketAddr;
 
+// echo server in rust using hyper and futures
 async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    match (req.method(), req.uri().path()) {
-        _ => {
-            debug!("Path: {}", req.uri());
-            debug!("Method: {}", req.method());
-            debug!("Headers: {:?}", req.headers());
-            debug!("Body: {:?}", hyper::body::to_bytes(req.into_body()).await?);
+    // debug logs the request
+    debug!("Path: {}", req.uri());
+    debug!("Method: {}", req.method());
+    debug!("Headers: {:?}", req.headers());
+    debug!("Body: {:?}", hyper::body::to_bytes(req.into_body()).await?);
 
-            let response = Response::builder()
-                .status(StatusCode::OK)
-                .header(header::CONTENT_TYPE, "application/json")
-                .body(Body::from("{}"))
-                .unwrap();
-            Ok(response)
-        }
-    }
+    // create a response with status code 200 and body "{} and content type application/json"
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(Body::from("{}"))
+        .unwrap();
+
+    // return the response
+    Ok(response)
 }
 
+// shutdown server gracefully
 async fn shutdown_signal() {
     tokio::signal::ctrl_c()
         .await
         .expect("failed to install CTRL+C signal handler");
 }
 
+// run server function with shutdown signal handler
 async fn run_server() {
-    let port;
-    match env::var("PORT") {
-        Ok(val) => port = val,
-        Err(_e) => port = "8080".to_string(),
+    // port to listen on from environment variable
+    let port = match env::var("PORT") {
+        Ok(val) => val,
+        Err(_e) => "8080".to_string(),
     };
 
+    // parse port u16
     let port = port.parse::<u16>().unwrap();
 
+    // env logger initialization
     env_logger::init();
+
     // We'll bind to 127.0.0.1:3000
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
 
@@ -49,6 +55,7 @@ async fn run_server() {
         Ok::<_, Infallible>(service_fn(echo))
     });
 
+    // Create the server
     let server = Server::bind(&addr).serve(make_svc);
 
     // And now add a graceful shutdown signal...
@@ -67,13 +74,16 @@ async fn main() {
     run_server().await;
 }
 
+// test module for echo server
 #[cfg(test)]
 mod test {
+    // import echo server
     use super::*;
     use hyper::{body::to_bytes, Client, Method};
     use tokio::runtime::Runtime;
     #[test]
     fn test_echo() {
+        // create a runtime
         let rt = Runtime::new().unwrap();
 
         // start server
@@ -82,6 +92,7 @@ mod test {
         // wait for server to come up
         std::thread::sleep(std::time::Duration::from_millis(50));
 
+        // create a client
         let client = Client::new();
 
         // make requests
@@ -93,11 +104,13 @@ mod test {
                 .unwrap(),
         );
 
+        // get response
         let res = rt.block_on(req).unwrap();
+
+        // get body
         let body = rt.block_on(to_bytes(res.into_body())).unwrap();
 
-        println!("{:?}", body);
-
+        // check response
         assert_eq!(std::str::from_utf8(&body).unwrap(), "{}");
     }
 }
